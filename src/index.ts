b258dd81face39bc45cdad9eada81697f860ea16
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-04-23 07:12:38 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import bodyParser from "body-parser";
 import LINE_HEADER from "./header";
 import hmacSHA256 from "crypto-js/hmac-sha256";
