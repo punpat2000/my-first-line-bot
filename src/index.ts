92b92bda22dd4e5da3cddafd444ffaca429a0a6a
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-04-27 18:21:38 +0700

    change syntax

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import validateSignature from './middlewares/validate-signature';
 import confirmCommunication from './middlewares/confirm-communication';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT ?? 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
