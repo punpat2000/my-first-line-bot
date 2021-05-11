799815332e850db620d4a008ae25fc71bc75ba09
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-05-11 10:35:14 +0700

    add quotation mark

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -40,7 +40,7 @@ async function reply(event: MessageEvent) {
 function toBinary(dec: string): string {
 	const x = +dec;
 	if (isNaN(x)) {
-		return `${dec} is not a number`;
+		return `"${dec}" is not a number`;
 	}
 	return (x >>> 0).toString(2);
 }
