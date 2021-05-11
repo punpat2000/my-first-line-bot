b9fe3a172924b652055a3b11abeab6c63ac6aa5d
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-05-11 11:24:15 +0700

    add validate request

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import listenToWebhook from './controllers/webhook.controller';
 import validateSignature from './middlewares/validate-signature';
 import confirmCommunication from './middlewares/confirm-communication';
+import validateRequest from './middlewares/validate-request';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(validateSignature);
 app.use(confirmCommunication);
+app.use(validateRequest);
 
 app.post('/webhook', listenToWebhook);
 
