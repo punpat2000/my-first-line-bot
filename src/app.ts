e5637f0fe461ba1e33e2f1a7fa28cccbe989889c
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-07-01 23:55:30 +0700

    bump packages and add a vscode setting

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import listenToWebhook from './controllers/webhook.controller';
 import { middlewares } from './middlewares';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(middlewares);
 
 app.post('/webhook', listenToWebhook);
